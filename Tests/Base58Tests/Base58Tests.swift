5ddba0f20ded6692692bddd75f4bb5d13b7d33d8
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2025-10-29 19:06:35 -0400

    added numbers to test

--- Tests/Base58Tests/Base58Tests.swift
+++ Tests/Base58Tests/Base58Tests.swift
@@ -22,8 +22,20 @@ import Testing
 
 @Test func decodeQuickBrownFox() {
     let input = "USm3fpXnKG5EUBx2ndxBDMPVciP5hGey2Jh4NDv6gmeo1LkMeiKrLJUUBk6Z"
-    let output = try! Base58.decode(input)
+    let output: [UInt8] = try! Base58.decode(input)
     #expect(output == Array("The quick brown fox jumps over the lazy dog.".utf8))
+}
+
+@Test func encodeWithNumbers() {
+    let input: [UInt8] = Array("123apple34".utf8)
+    let output = Base58.encode(input)
+    #expect(output == "3mJr8tDaz2NEKM")
+
+}
+@Test func decodeWithNumbers() {
+    let input = "3mJr8tDaz2NEKM"
+    let output = try! Base58.decode(input)
+    #expect(output == Array("123apple34".utf8))
 }
 
 @Test func encodeEmpty() {
